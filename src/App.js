b9381bf5471e81c92e84b26433c7bd33be49e565
Dylan <chan0155@LAPTOP-00DPFMK2>
2025-02-24 19:00:28 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is a placeholder for my portfolio website.</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js', // Entry point for your app
+  output: {
+    path: path.resolve(__dirname, 'dist'), // Output directory
+    filename: 'bundle.js', // Output file
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/, // Apply Babel to all .js files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html', // HTML template
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'), // Serve files from the dist folder
+    },
+    compress: true,
+    port: 3000, // Port for the dev server
+  },
+};
