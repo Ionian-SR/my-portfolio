880bfac626ef99fa9218eff96ea329b66fea007c
Dylan <chan0155@LAPTOP-00DPFMK2>
2025-03-21 16:37:08 -0500

    initial template home page

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/index.js', // Entry point for your app
   output: {
-    path: path.resolve(__dirname, 'dist'), // Output directory
+    path: path.resolve(__dirname, 'build'), // Output directory
     filename: 'bundle.js', // Output file
   },
   module: {
